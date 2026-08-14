03858ce502c9c3dfdccbd2e2e1f726f3cd5a1bbd
Luo <2508015296@qq.com>
2026-08-14 14:22:55 +0800

    chore(tooling): 使用默认 lint 规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,3 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config';
 
-export default [
-  ...(await createConfigForNuxt({
-    features: {
-      tooling: true,
-      stylistic: true,
-    },
-  })),
-  {
-    rules: {
-      'vue/multi-word-component-names': 'off',
-    },
-  },
-];
+export default [...(await createConfigForNuxt())];
